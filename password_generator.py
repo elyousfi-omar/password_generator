fba4cf0d292080bcf491b7e0acd1c6ae5937e1a9
Omar El Yousfi <49985464+elyousfi-omar@users.noreply.github.com>
2022-01-05 17:43:43 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,24 +2,22 @@ from string import *
 from random import sample
 import sys
 
-class passwords:
+def random_password(k):
+	"""Generates a random password using digits, ascii letters, and ponctuations
 
-	def random_password(k):
-		"""Generates a random password using digits, ascii letters, and ponctuations
+	Args:
+		k (int): Number of characters in the password
+	Returns:
+		string password
+	"""
+	digit = digits
+	lower_case = ascii_lowercase
+	upper_case = ascii_uppercase
+	ponct = punctuation
+	password = digits + lower_case + upper_case + ponct 
 
-		Args:
-			k (int): Number of characters in the password
-		Returns:
-			string password
-		"""
-		digit = digits
-		lower_case = ascii_lowercase
-		upper_case = ascii_uppercase
-		ponct = punctuation
-		password = digits + lower_case + upper_case + ponct 
-
-		return(''.join(sample(list(password), int(k))))
+	return(''.join(sample(list(password), int(k))))
 
 
 if __name__ == '__main__':
-	 globals()[sys.argv[1]](sys.argv[2])
+	print(globals()[sys.argv[1]](sys.argv[2]))
