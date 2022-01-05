0f1808bf88e3b85d5599636376faaca2c43a0498
Omar El Yousfi <49985464+elyousfi-omar@users.noreply.github.com>
2022-01-05 17:37:54 +0100

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+from string import *
+from random import sample
+import sys
+
+class passwords:
+
+	def random_password(k):
+		"""Generates a random password using digits, ascii letters, and ponctuations
+
+		Args:
+			k (int): Number of characters in the password
+		Returns:
+			string password
+		"""
+		digit = digits
+		lower_case = ascii_lowercase
+		upper_case = ascii_uppercase
+		ponct = punctuation
+		password = digits + lower_case + upper_case + ponct 
+
+		return(''.join(sample(list(password), int(k))))
+
+
+if __name__ == '__main__':
+	 globals()[sys.argv[1]](sys.argv[2])
